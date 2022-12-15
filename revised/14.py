315af0e7650d220f0731c215bf9715b1728d4977
P-aradox <dominic.peel@warwick.ac.uk>
2022-12-15 03:33:01 +0000

    Revised Day14

--- revised/14.py
+++ revised/14.py
@@ -1,68 +1,41 @@
 data = open("14.txt").read().strip()
-rock_shape = [[[int(z) for z in y.split(",")] for y in x.split(" -> ")] for x in data.split("\n")] 
+lines = [[[int(z) for z in y.split(",")] for y in x.split(" -> ")] for x in data.split("\n")] 
 
-rocksArr = []
+rocks = set()
 max_rock = 0
-for rock_form in rock_shape:
-	for i in range(len(rock_form)-1):
-		a, b = rock_form[i], rock_form[i+1]
-		for k in range(min(a[0], b[0]), max(a[0], b[0])+1):
-			rocksArr.append((k, a[1]))
-		for k in range(min(a[1], b[1]), max(a[1], b[1])+1):
-			rocksArr.append((a[0], k))
-		max_rock = max(max_rock, a[1], b[1])
+for line in lines:
+	for (x1, y1), (x2, y2) in zip(line, line[1:]):
+		x1, x2 = sorted([x1, x2])
+		y1, y2 = sorted([y1, y2])
+		for x in range(x1, x2+1):
+			for y in range(y1, y2+1):
+				rocks.add(x + y*1j)
+				if y > max_rock:
+					max_rock = y
 
-solid = set(rocksArr)
-
-# Part 1
-sand = (500,0)
-abyss = False
-i = 0
-while True:
-	x, y = sand
-	while True:
-		if y == max_rock:
-			abyss = True
-			break
-		elif (x, y+1) not in solid:
-			x, y = x, y+1
-		elif (x-1, y+1) not in solid:
-			x, y = x-1, y+1
-		elif (x+1, y+1) not in solid:
-			x, y = x+1, y+1
-		else:
-			solid.add((x, y))
-			break
-	if abyss:
-		break
-	i += 1
-
-print(i)
-
-# Part 2
-solid = set(rocksArr)
 floor = max_rock + 2
-blocked = False
+blocked = abyss = False
 i = 0
 while True:
-	x, y = sand
+	x = 500
 	while True:
-		if y+1 == floor:
-			solid.add((x, y))
+		if not abyss and x.imag == max_rock:
+			print(i) # Part 1
+			abyss = True
+		if x.imag + 1 == floor:
 			break
-		elif (x, y+1) not in solid:
-			x, y = x, y+1
-		elif (x-1, y+1) not in solid:
-			x, y = x-1, y+1
-		elif (x+1, y+1) not in solid:
-			x, y = x+1, y+1
+		elif x + 1j not in rocks:
+			x += 1j
+		elif x-1 + 1j not in rocks:
+			x += 1j - 1
+		elif x+1 + 1j not in rocks:
+			x += 1j + 1
 		else:
-			if y == 0:
+			if x.imag == 0:
 				blocked = True
-			solid.add((x, y))
 			break
+	rocks.add(x)
 	i += 1
 	if blocked:
+		print(i) # Part 2
 		break
-
-print(i)
